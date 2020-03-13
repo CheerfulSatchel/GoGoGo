8a2c56741b8adfe34828055e443f3306941bfc20
James Wu <therealjameswu@gmail.com>
2020-03-13 19:02:50 -0400

    Added database package.

---
+++ networking/pseudonym/database/database.go
@@ -0,0 +1,84 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-pg/pg/orm"
+
+	"github.com/go-pg/pg"
+)
+
+type customError struct {
+	errorMessage string
+}
+
+type Pseudonym struct {
+	tableName struct{} `sql:"pseudonym"`
+	ID        int
+	Username  string
+	Details   *PseudonymDetails
+}
+
+type PseudonymDetails struct {
+	tableName   struct{} `sql:"pseudonymdetails`
+	HTMLURL     string
+	ID          int
+	Likes       int
+	PseudonymID int
+	Pseudonym   *Pseudonym
+}
+
+var pgdb *pg.DB
+
+func init() {
+	pgdb = connect()
+	fmt.Println("Connected to database~")
+	fmt.Println(pgdb)
+}
+
+func connect() *pg.DB {
+	return pg.Connect(&pg.Options{
+		User:     os.Getenv("PSEUDONYM_USER"),
+		Password: os.Getenv("PSEUDONYM_PASSWORD"),
+		Database: os.Getenv("PSEUDONYM_DATABASE"),
+	})
+}
+
+func (e *customError) Error() string {
+	return e.errorMessage
+}
+
+func CreateTables() error {
+	for _, model := range []interface{}{&Pseudonym{}, &PseudonymDetails{}} {
+		err := pgdb.CreateTable(model, &orm.CreateTableOptions{
+			FKConstraints: true,
+		})
+		if err != nil {
+			fmt.Println(err)
+			return &customError{"Failure to create tables! "}
+		}
+	}
+
+	fmt.Println("Successfully created tables!!")
+	return nil
+}
+
+func Query() {
+
+	fmt.Println("YOOO")
+	fmt.Println(pgdb)
+	// var testperson []testpeople
+	// aUser := &Gituser{ID: 2, Name: "bob"}
+	// err := pgdb.Insert(aUser)
+	// fmt.Println(err)
+	// err := pgdb.Select(aUser)
+
+	// if err != nil {
+	// 	fmt.Printf("ERROR... %v", err)
+	// } else {
+	// 	fmt.Println(aUser)
+	// 	fmt.Println("ALL DONE :]")
+	// }
+
+}
