3613542ce3bdbd4c9977988055bd1dd921d35519
James Wu <therealjameswu@gmail.com>
2020-03-13 19:04:20 -0400

    Added base add entry endpoint.

--- networking/pseudonym/server/server.go
+++ networking/pseudonym/server/server.go
@@ -1,18 +1,46 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/CheerfulSatchel/GoGoGo/networking/pseudonym/database"
 )
+
+type Response struct {
+	Message string
+}
 
 func all(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("INCOMING REQUEST: %v", r.Method)
 	io.WriteString(w, "Ur mum\n")
+	database.Query()
+}
+
+func addEntries(w http.ResponseWriter, r *http.Request) {
+	response := Response{""}
+	if r.Method == "POST" {
+		response.Message = "Yay! Added new users to the database!"
+		w.WriteHeader(http.StatusOK)
+	} else {
+		response.Message = "Sorry, GET method not accepted here :["
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	js, err := json.MarshalIndent(response, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func StartServer() {
 	http.HandleFunc("/all", all)
+	http.HandleFunc("/add", addEntries)
 
 	http.ListenAndServe(":8080", nil)
 }
