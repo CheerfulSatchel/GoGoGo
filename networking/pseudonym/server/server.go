823beb72bbff037a4fd94200472596d06e2a26f7
James Wu <therealjameswu@gmail.com>
2020-03-16 23:15:53 -0400

    Updated check for http method type to use function.

--- networking/pseudonym/server/server.go
+++ networking/pseudonym/server/server.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/CheerfulSatchel/GoGoGo/networking/pseudonym/database"
 	"github.com/CheerfulSatchel/GoGoGo/networking/pseudonym/gitcrawler"
@@ -25,8 +27,7 @@ func addEntries(w http.ResponseWriter, r *http.Request) {
 	response := Response{"", nil}
 	httpReturnCode := http.StatusOK
 
-	if r.Method == "POST" {
-
+	if err := verifyMethodType(r.Method, "POST", &response, &w); err == nil {
 		randomGitusers, getRandomGitusersErr := gitcrawler.GetRandomUsers()
 		if getRandomGitusersErr != nil {
 			response.Message = "Failed to get random users..."
@@ -46,29 +47,25 @@ func addEntries(w http.ResponseWriter, r *http.Request) {
 				fmt.Println("Inserting user " + newPseudonym.Username)
 				userErr := database.InsertUserIntoTable(newPseudonym)
 				if userErr != nil {
-					response.Message = "Failed to insert user " + newPseudonym.Username
+					response.Message = fmt.Sprintf("Failed to insert user %s", newPseudonym.Username)
 					response.Err = userErr
 				} else {
-					response.Message = "YAY! Inserted user " + newPseudonym.Username
+					response.Message = fmt.Sprintf("YAY! Inserted user %s", newPseudonym.Username)
 				}
 
 				fmt.Println("Inserting user detail for user " + newPseudonymDetails.Pseudonym.Username)
+				newPseudonymDetails.PseudonymID = newPseudonym.ID
 				userDetailsErr := database.InsertUserDetailIntoTable(newPseudonymDetails)
 				if userDetailsErr != nil {
-					response.Message = "Failed to insert user " + newPseudonym.Username
+					response.Message = fmt.Sprintf("Failed to insert user %s", newPseudonym.Username)
 					response.Err = userDetailsErr
 				} else {
-					response.Message = "YAY! Inserted user " + newPseudonym.Username
+					response.Message = fmt.Sprintf("YAY! Inserted user %s", newPseudonym.Username)
 				}
 			}
 		}
-
-	} else {
-		response.Message = "Sorry, GET method not accepted here :["
-		httpReturnCode = http.StatusBadRequest
+		w.WriteHeader(httpReturnCode)
 	}
-
-	w.WriteHeader(httpReturnCode)
 
 	js, err := json.MarshalIndent(response, "", " ")
 	if err != nil {
@@ -79,9 +76,37 @@ func addEntries(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+func retrieveUser(w http.ResponseWriter, r *http.Request) {
+	response := Response{"", nil}
+	httpReturnCode := http.StatusOK
+
+	if err := verifyMethodType(r.Method, "GET", &response, &w); err == nil {
+
+		w.WriteHeader(httpReturnCode)
+	}
+	js, err := json.MarshalIndent(response, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func verifyMethodType(givenMethod, acceptedMethod string, response *Response, w *http.ResponseWriter) error {
+	if strings.Compare(givenMethod, acceptedMethod) == 0 {
+		return nil
+	} else {
+		response.Message = fmt.Sprintf("Sorry, given method %s is not accepted.", givenMethod)
+		(*w).WriteHeader(http.StatusBadRequest)
+		return errors.New(response.Message)
+	}
+}
+
 func StartServer() {
 	http.HandleFunc("/all", all)
 	http.HandleFunc("/add", addEntries)
+	http.HandleFunc("/retrieve", retrieveUser)
 
 	http.ListenAndServe(":8080", nil)
 }
