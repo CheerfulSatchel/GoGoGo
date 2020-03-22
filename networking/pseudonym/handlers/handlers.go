75f81d8c479a1bcce91aa9b563d4cc5958385812
James Wu <therealjameswu@gmail.com>
2020-03-22 17:11:48 -0400

    Refactored all route handler functions.

--- networking/pseudonym/handlers/handlers.go
+++ networking/pseudonym/handlers/handlers.go
@@ -12,100 +12,121 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type Response struct {
-	Message string
-	Err     error
-}
+type Response map[string]interface{}
 
 func AllPseudonyms(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Printf("INCOMING REQUEST: %v", r.Method)
 	io.WriteString(w, "Ur mum\n")
 }
 
-func AddPseudonym(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	response := Response{"", nil}
-	httpReturnCode := http.StatusOK
-
-	if err := verifyMethodType(r.Method, "POST", &response, &w); err == nil {
-		randomGitusers, getRandomGitusersErr := gitcrawler.GetRandomUsers()
-		if getRandomGitusersErr != nil {
-			response.Message = "Failed to get random users..."
-			response.Err = getRandomGitusersErr
-		} else {
-			for _, randomGitusers := range *randomGitusers {
-				newPseudonym := &database.Pseudonym{
-					Username: randomGitusers.Login,
-				}
-
-				newPseudonymDetails := &database.PseudonymDetails{
-					HTMLURL:   randomGitusers.HTMLURL,
-					Likes:     0,
-					Pseudonym: newPseudonym,
-				}
-
-				fmt.Println("Inserting user " + newPseudonym.Username)
-				userErr := database.InsertUserIntoTable(newPseudonym)
-				if userErr != nil {
-					response.Message = fmt.Sprintf("Failed to insert user %s", newPseudonym.Username)
-					response.Err = userErr
-				} else {
-					response.Message = fmt.Sprintf("YAY! Inserted user %s", newPseudonym.Username)
-				}
-
-				fmt.Println("Inserting user detail for user " + newPseudonymDetails.Pseudonym.Username)
-				newPseudonymDetails.PseudonymID = newPseudonym.ID
-				userDetailsErr := database.InsertUserDetailIntoTable(newPseudonymDetails)
-				if userDetailsErr != nil {
-					response.Message = fmt.Sprintf("Failed to insert user %s", newPseudonym.Username)
-					response.Err = userDetailsErr
-				} else {
-					response.Message = fmt.Sprintf("YAY! Inserted user %s", newPseudonym.Username)
-				}
-			}
-		}
-		w.WriteHeader(httpReturnCode)
+func createResponse(message string, err error, data interface{}) *Response {
+	return &Response{
+		"message": message,
+		"err":     err,
+		"data":    data,
 	}
+}
 
-	js, err := json.MarshalIndent(response, "", " ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func (response *Response) ToJSON(w io.Writer) error {
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", " ")
+
+	return encoder.Encode(response)
+}
+
+func CreatePseudonyms(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	verifyMethodTypeErr := verifyMethodType(r.Method, http.MethodPost, &w)
+
+	if verifyMethodTypeErr != nil {
+		http.Error(w, verifyMethodTypeErr.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	randomGitusers, getRandomGitusersErr := gitcrawler.GetRandomUsers()
+	if getRandomGitusersErr != nil {
+		http.Error(w, getRandomGitusersErr.Error(), http.StatusFailedDependency)
+		return
+	}
+
+	addedUsers := make([]*database.PseudonymDetails, 2)
+
+	for _, randomGitusers := range *randomGitusers {
+		newPseudonym := &database.Pseudonym{
+			Username: randomGitusers.Login,
+		}
+
+		newPseudonymDetails := &database.PseudonymDetails{
+			HTMLURL:   randomGitusers.HTMLURL,
+			Likes:     0,
+			Pseudonym: newPseudonym,
+		}
+
+		fmt.Printf("Inserting user %v\n", newPseudonym.Username)
+		userErr := database.InsertUserIntoTable(newPseudonym)
+		if userErr != nil {
+			fmt.Printf("Failed to insert user %v\n", newPseudonym.Username)
+		} else {
+			fmt.Printf("YAY! Inserted user %v\n", newPseudonym.Username)
+		}
+
+		fmt.Printf("Inserting user detail for user %v\n", newPseudonymDetails.Pseudonym.Username)
+		newPseudonymDetails.PseudonymID = newPseudonym.ID
+		userDetailsErr := database.InsertUserDetailIntoTable(newPseudonymDetails)
+		if userDetailsErr != nil {
+			fmt.Printf("Failed to insert user %v\n", newPseudonym.Username)
+		} else {
+			fmt.Printf("YAY! Inserted user %v\n", newPseudonym.Username)
+			addedUsers = append(addedUsers, newPseudonymDetails)
+		}
+	}
+
+	response := createResponse(
+		fmt.Sprintf("Successfully created %v users", len(addedUsers)),
+		nil,
+		addedUsers)
+
+	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	toJSONErr := response.ToJSON(w)
+
+	if toJSONErr != nil {
+		http.Error(w, "Failure to encode JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func GetPseudonym(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	response := Response{"", nil}
-	httpReturnCode := http.StatusOK
+	verifyMethodTypeErr := verifyMethodType(r.Method, http.MethodGet, &w)
 
-	if err := verifyMethodType(r.Method, "GET", &response, &w); err == nil {
-		w.WriteHeader(httpReturnCode)
-		givenIDInt, convertGivenIDErr := strconv.Atoi(ps.ByName("id"))
-		if convertGivenIDErr != nil {
-			fmt.Println("Error converting the given id to an integer!")
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		} else {
-			pseudonymDetails, queryErr := database.Query(givenIDInt)
-			if queryErr != nil {
-				fmt.Println("Error querying id " + string(givenIDInt))
-				http.Error(w, err.Error(), http.StatusBadRequest)
-				return
-			} else {
-				bytes, _ := json.Marshal(pseudonymDetails)
-				response.Message = fmt.Sprintf("%v", string(bytes))
-				response.Err = nil
-			}
-
-		}
-
-	}
-	js, err := json.MarshalIndent(response, "", " ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if verifyMethodTypeErr != nil {
+		http.Error(w, verifyMethodTypeErr.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	givenIDInt, convertGivenIDErr := strconv.Atoi(ps.ByName("id"))
+	if convertGivenIDErr != nil {
+		http.Error(w, convertGivenIDErr.Error(), http.StatusBadRequest)
+		return
+	}
+
+	pseudonymDetails, queryErr := database.Query(givenIDInt)
+	if queryErr != nil {
+		http.Error(w, queryErr.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := createResponse(
+		fmt.Sprintf("Successfully retrieved %v", pseudonymDetails.Pseudonym.Username),
+		nil,
+		pseudonymDetails)
+
+	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	toJSONErr := response.ToJSON(w)
+
+	if toJSONErr != nil {
+		http.Error(w, "Failure to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
 }
