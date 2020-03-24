af6a867ede622b929da6e2dac6fc30fc6b1f3155
James Wu <therealjameswu@gmail.com>
2020-03-24 18:45:46 -0400

    Added handler for AddPseudonym.

--- networking/pseudonym/database_service/handlers/handlers.go
+++ networking/pseudonym/database_service/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/CheerfulSatchel/GoGoGo/networking/pseudonym/database_service/database"
+	"github.com/CheerfulSatchel/GoGoGo/networking/pseudonym/database_service/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -52,7 +53,47 @@ func CreateTables(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	toJSONErr := response.ToJSON(w)
 
 	if toJSONErr != nil {
-		http.Error(w, "Failure to encode JSON", http.StatusInternalServerError)
+		http.Error(w, "Failure to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func AddPseudonym(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	verifyMethodTypeErr := verifyMethodType(r.Method, http.MethodPut, &w)
+
+	if verifyMethodTypeErr != nil {
+		http.Error(w, verifyMethodTypeErr.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	pseudonym := new(models.Pseudonym)
+
+	defer r.Body.Close()
+	decoderErr := pseudonym.FromJSON(r.Body)
+
+	if decoderErr != nil {
+		http.Error(w, decoderErr.Error(), http.StatusBadRequest)
+		return
+	}
+
+	insertPseudonymErr := database.InsertPseudonym(pseudonym)
+
+	if insertPseudonymErr != nil {
+		http.Error(w, insertPseudonymErr.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := createResponse(
+		fmt.Sprintf("Successfully added pseudonym~"),
+		nil,
+		pseudonym)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	toJSONErr := response.ToJSON(w)
+
+	if toJSONErr != nil {
+		http.Error(w, "Failure to encode response", http.StatusInternalServerError)
 		return
 	}
 }
