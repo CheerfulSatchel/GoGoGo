789ead31b3d2a5627407242f17e13669c502f866
James Wu <therealjameswu@gmail.com>
2020-03-24 18:45:18 -0400

    Added FromJSON method.

--- networking/pseudonym/database_service/models/database_models.go
+++ networking/pseudonym/database_service/models/database_models.go
@@ -1,16 +1,19 @@
 package models
 
+import (
+	"encoding/json"
+	"io"
+)
+
 type Pseudonym struct {
 	tableName struct{} `sql:"pseudonym"`
 	ID        int      `json:"id"`
 	Username  string   `json:"username"`
+	HTMLURL   string   `json:"html_url"`
+	Likes     int      `json:"likes" pg:",use_zero"`
 }
 
-type PseudonymDetails struct {
-	tableName   struct{}   `sql:"pseudonymdetails`
-	HTMLURL     string     `json:"html_url"`
-	ID          int        `json:"details_id"`
-	Likes       int        `json:"likes"`
-	PseudonymID int        `json:"-"`
-	Pseudonym   *Pseudonym `json:"pseudonym"`
+func (pseudonym *Pseudonym) FromJSON(r io.Reader) error {
+	decoder := json.NewDecoder(r)
+	return decoder.Decode(pseudonym)
 }
